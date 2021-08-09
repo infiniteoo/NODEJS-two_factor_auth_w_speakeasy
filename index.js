82abaea344f49df7b0ba735e9793ad633c88c7e8
Troy Dorman <troydorman@gmail.com>
2021-08-08 20:28:51 -0600

    created post route to verify a a temp secret code with google authenticator into a permanent secret code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { JsonDB } = require("node-json-db");
 const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
 
 const app = express();
+app.use(express.json());
 
 const db = new JsonDB(new Config("myDatabase", true, false, "/"));
 
@@ -25,6 +26,31 @@ app.post("/api/register", (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error generating the secret" });
+  }
+});
+
+// verify token and make secret permanent
+app.post("/api/verify", (req, res) => {
+  const { token, userId } = req.body;
+  try {
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+    const { base32: secret } = user.temp_secret;
+    const verified = speakeasy.totp.verify({
+      secret,
+      encoding: "base32",
+      token: token,
+    });
+
+    if (verified) {
+      db.push(path, { id: userId, secret: user.temp_secret });
+      res.json({ verified: true });
+    } else {
+      res.json({ verified: false });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error finding user" });
   }
 });
 
