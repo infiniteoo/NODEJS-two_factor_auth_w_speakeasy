4e16c4a256e8006613652dc775b226ae43512635
Troy Dorman <troydorman@gmail.com>
2021-08-08 20:40:01 -0600

    cleaned up some code

--- index.js
+++ index.js
@@ -1,84 +1,95 @@
 const express = require("express");
-const speakeasy = require("speakeasy");
+const bodyParser = require("body-parser");
+const JsonDB = require("node-json-db").JsonDB;
+const Config = require("node-json-db/dist/lib/JsonDBConfig").Config;
 const uuid = require("uuid");
-const { JsonDB } = require("node-json-db");
-const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
+const speakeasy = require("speakeasy");
 
 const app = express();
-app.use(express.json());
 
-const db = new JsonDB(new Config("myDatabase", true, false, "/"));
+// The second argument is used to tell the DB to save after each push
+// If you put false, you'll have to call the save() method.
+// The third argument is to ask JsonDB to save the database in an human readable format. (default false)
+// The last argument is the separator. By default it's slash (/)
+var db = new JsonDB(new Config("myDataBase", true, false, "/"));
 
-app.get("/api", (req, res) =>
-  res.json({ message: "welcome to the 2FA example" })
-);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// register user & create temporary secret
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to the two factor authentication exmaple" });
+});
+
 app.post("/api/register", (req, res) => {
   const id = uuid.v4();
-
   try {
     const path = `/user/${id}`;
+    // Create temporary secret until it it verified
     const temp_secret = speakeasy.generateSecret();
-    console.log(temp_secret);
+    // Create user in the database
     db.push(path, { id, temp_secret });
+    // Send user id and base32 key to user
     res.json({ id, secret: temp_secret.base32 });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error generating the secret" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Error generating secret key" });
   }
 });
 
-// verify token and make secret permanent
 app.post("/api/verify", (req, res) => {
-  const { token, userId } = req.body;
+  const { userId, token } = req.body;
   try {
+    // Retrieve user from database
     const path = `/user/${userId}`;
     const user = db.getData(path);
+    console.log({ user });
     const { base32: secret } = user.temp_secret;
     const verified = speakeasy.totp.verify({
       secret,
       encoding: "base32",
-      token: token,
+      token,
     });
-
     if (verified) {
+      // Update user data
       db.push(path, { id: userId, secret: user.temp_secret });
       res.json({ verified: true });
     } else {
       res.json({ verified: false });
     }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error finding user" });
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving user" });
   }
 });
 
-// validate token
 app.post("/api/validate", (req, res) => {
-  const { token, userId } = req.body;
+  const { userId, token } = req.body;
   try {
+    // Retrieve user from database
     const path = `/user/${userId}`;
     const user = db.getData(path);
+    console.log({ user });
     const { base32: secret } = user.secret;
-    const tokenValidate = speakeasy.totp.verify({
+    // Returns true if the token matches
+    const tokenValidates = speakeasy.totp.verify({
       secret,
       encoding: "base32",
-      token: token,
+      token,
       window: 1,
     });
-
     if (tokenValidates) {
       res.json({ validated: true });
     } else {
       res.json({ validated: false });
     }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error finding user" });
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving user" });
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const port = 9000;
 
-app.listen(PORT, () => console.log(`Server is live on port ${PORT}`));
+app.listen(port, () => {
+  console.log(`App is running on PORT: ${port}.`);
+});
